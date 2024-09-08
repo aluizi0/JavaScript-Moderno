40d8ed4e3782e14d35f98b452c82d3fa18a7c184
Aluízio Oliveira Gonçalves Filho <80135392+aluizi0@users.noreply.github.com>
2024-09-08 12:00:47 -0300

    implementing victory or draw for one of the players

--- Exercicio13-Jogo_da_Velha/index.js
+++ Exercicio13-Jogo_da_Velha/index.js
@@ -15,15 +15,53 @@ function initializeGame() {
     boardRegions.forEach(function (element) {
         element.classList.remove('win');
         element.innerText = '';
+        element.classList.add('cursor-pointer')
         element.addEventListener('click', handleBoardClick);
     });
 }
 
+function getWinRegions(){
+    const winRegions = []
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2]){
+        winRegions.push('0.0', '0.1', '0.2')
+    }
+    if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2]){
+        winRegions.push('1.0', '1.1', '1.2')
+    }
+    if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2]){
+        winRegions.push('2.0', '2.1', '2.2')
+    }
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0]){
+        winRegions.push('0.0', '1.0', '2.0')
+    }
+    if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1]){
+        winRegions.push('0.1', '1.1', '2.1')
+    }
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2]){
+        winRegions.push('0.2', '1.2', '2.2')
+    }
+    if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2]){
+        winRegions.push('0.0', '1.1', '2.2')
+    }
+    if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0]){
+        winRegions.push('0.2', '1.1', '2.0')
+    }
+
+    return winRegions
+}
+
 // Função para desabilitar uma região do tabuleiro marcada
 function disableRegion(element){
-    element.style.cursor = 'default';
+    element.classList.remove('cursor-pointer')
     element.removeEventListener('click', handleBoardClick);
+}
 
+function handleWin(regions){
+    regions.forEach(function(region){
+        document.querySelector('[data-region="' + region + '"]').classList.add('win')
+    })
+    const playerName = document.getElementById(turnPlayer).value
+    document.querySelector('h2').innerHTML = playerName + ' Venceu! '
 }
 
 // Função para lidar com o clique no tabuleiro
@@ -49,6 +87,16 @@ function handleBoardClick(ev) {
 
     // Chamando função de desbilitar regiao ja marcada no tabuleiro
     disableRegion(span)
+
+    const winRegions = getWinRegions()
+    if (winRegions.length > 0){
+        handleWin(winRegions)
+    } else if (vBoard.flat().includes('')){
+        turnPlayer = turnPlayer === 'player1' ? 'player2' : 'player1'
+        uptadeTitle()
+    } else{
+        document.querySelector('h2').innerHTML = 'Empate!'
+    }
 }
 
 // Adiciona o evento de clique ao botão de iniciar o jogo
