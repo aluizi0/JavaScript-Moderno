4749a1be5893ef938d32b9cedf5c31e2eb3ae5c9
Aluízio Oliveira Gonçalves Filho <80135392+aluizi0@users.noreply.github.com>
2024-09-15 22:37:09 -0300

    template literals

---
+++ 03-template-literals/index.js
@@ -0,0 +1,6 @@
+const name = 'Aluizio';
+
+// Usando template literals e aprendendo metodos 
+console.log(`Hello, ${name}!
+Estou aprendendo JavaScript em ${new Date().getFullYear()}.
+2+2 = ${2 + 2}.`);
